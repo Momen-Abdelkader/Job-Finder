8d1a07c984b85dd54ac6ce55964765cd41408162
Hassan Ali <162273873+HassanIsmail16@users.noreply.github.com>
2025-04-19 01:04:53 +0200

    implemented nav hamburger menu

--- js/nav.js
+++ js/nav.js
@@ -1,20 +1,21 @@
 function createNav(isLoggedIn = false, isAdmin = false) {
     let navHtml = `
     <nav class="main-nav">
-        <ul class="nav-list">   
-            <a href="home.html" class="logo">Job Finder</a>
-            <li><a href="Home.html">Home</a></li>
-            ${isAdmin ? `<li><a href="admin.html">Dashboard</a></li>` : `<li><a href="jobs.html">Jobs</a></li> <li>`}
-        </ul>
-        ${isLoggedIn ? `
-        <div class="profile" id="profile-nav">
-            <img src="../assets/temp-profile.webp" class="profile-image">
+        <a href="home.html" class="logo">Job Finder</a>
+        <div class="nav-content">
+            <ul class="nav-list">   
+                <li><a href="home.html">Home</a></li>
+                ${isAdmin ? '<li><a href="admin.html">Dashboard</a></li>' : '<li><a href="jobs.html">Jobs</a></li>'}
+            </ul>
+            ${isLoggedIn ? 
+            `<div class="profile" id="profile-nav">
+                <img src="../assets/temp-profile.webp" class="profile-image">
+            </div>` : 
+            `<div class="auth-buttons">
+                <a href="login.html" class="login">Login</a>
+                <a href="signup.html" class="signup">Sign Up</a>
+            </div>`}
         </div>
-        ` : `
-        <div class="auth-buttons">
-            <a href="login.html" class="login">Login</a>
-            <a href="signup.html" class="signup">Sign Up</a>
-        </div>`}
         <div class="menu-toggle">
             <span></span>
             <span></span>
@@ -30,4 +31,14 @@ function createNav(isLoggedIn = false, isAdmin = false) {
             link.classList.add('active');
         }
     });
+
+    const menuToggle = document.querySelector('.menu-toggle');
+    const authButtons = document.querySelector('.auth-buttons');
+    const navContent = document.querySelector('.nav-content');
+    menuToggle.addEventListener('click', () => {
+        navContent.classList.toggle('active');
+        authButtons.classList.toggle('active');
+        menuToggle.classList.toggle('active');
+    });
+
 }
