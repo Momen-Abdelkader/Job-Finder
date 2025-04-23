5d81cad0bca85eb73022e912250c8d7fdbd6a963
Mohamed Ali <mohamed.ali15122004@gmail.com>
2025-04-23 13:18:57 +0200

    added functionality to add button and fix merge conflict

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 createNav(true, true);
 
-const jobData = [
+var jobData = [
     {
         logo: "../assets/google.png",
         company: "Google",
@@ -27,9 +27,97 @@ const jobData = [
     }
 ];
 
-const jobCardsContainer = document.querySelector('.job-cards');
+function renderJobs() {
+    const jobCardsContainer = document.querySelector('.job-cards');
+    if(jobCardsContainer.innerHTML.trim() !== ''){
+        jobCardsContainer.innerHTML = '';
+    }
+  
+    jobData.forEach(job => {
+        const card = createJobCard(job, 'admin');
+        jobCardsContainer.appendChild(card);
+  });
+  }
 
-jobData.forEach(job => {
-    const card = createJobCard(job, 'admin');
-    jobCardsContainer.appendChild(card);
-});
+renderJobs();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const addButton = document.querySelector('.add_button');
+    const modal = document.querySelector('.modal');
+    const closeBtn = document.querySelector('.close');
+    const jobForm = document.querySelector('.job-form');
+    // const editButton = document.querySelectorAll('.edit-button button');
+    let isEdit = false;
+
+    const workLocation = document.querySelector('input[name="workLocation"]:checked').value;
+    const jobType = document.querySelector('input[name="jobType"]:checked').value;
+    const experience = document.querySelector('input[name="experience"]:checked').value;
+    
+    addButton.addEventListener('click', function() {
+      modal.style.display = 'block';
+    });
+    
+    closeBtn.addEventListener('click', function() {
+      modal.style.display = 'none';
+    });
+
+    // editButton.addEventListener('click', function() {
+    //   modal.style.display = 'block';
+    //   isEdit = true;
+    //   //document.querySelector("#logo").value = document.querySelector()
+    // });
+
+    document.querySelectorAll('.edit-button').forEach(button => {
+      button.addEventListener('click', function() {
+        var jobCard = this.closest('.job-card'); 
+        console.log(jobCard); 
+        let jobTags = jobCard.querySelectorAll('.tag'); 
+        const jobEdit = {
+          logo: jobCard.querySelector('.company-logo').src,
+          company: jobCard.querySelector('.company-name').innerHTML,
+          title: jobCard.querySelector('.job-title').innerHTML,
+          location: jobCard.querySelector('.job-location').innerHTML,
+          tags: [jobTags[0].innerHTML, jobTags[1].innerHTML, jobTags[2].innerHTML],
+          salary: jobCard[3].innerHTML
+        };
+
+        for (const job of jobData) {
+          if(job.logo == jobEdit.logo
+            && job.company == jobEdit.company
+            && job.location == jobEdit.location
+            && job.title == jobEdit.title
+            && job.tags == jobEdit.tags
+            && job.salary == jobEdit.salary){
+              break;
+          }
+        }
+
+      });
+    });
+
+    
+    jobForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+      
+      const newJob = {
+        logo: document.querySelector("#logo").value,
+        company: document.querySelector("#company-name").value,
+        title: document.querySelector('#job-title').value,
+        location: document.querySelector('#job-location').value,
+        tags: [workLocation, jobType, experience],
+        salary:  "$" + document.querySelector('#job-salary').value  + "/month",
+      };
+      
+      jobData.push(newJob);
+      renderJobs();
+
+      jobForm.reset();
+      modal.style.display = 'none';
+    });
+
+  });
+
+function deleteJob(id) {
+  jobs = jobs.filter(job => job.id !== id);
+  renderJobs();
+}
